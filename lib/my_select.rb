ee304db04ad90ff610e97c6f7002e2e9acb011a5
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-16 06:05:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
  i = 0
  new_array = []
  while i < collection.length
-   yield(collection[i]) == true
-    if new_array << collection[i]
+   if yield(collection[i]) == true
+    new_array << collection[i]
    i = i + 1
  end
  new_array
